b5b410b627a47f10bb08b380e929f23686c5ea96
1q4e1wd211 <kudaiberdi0806@gmail.com>
2023-09-05 16:57:42 +0600

    я удалил весь код и начал заново и сделал нав, но там появляется нижний скролинг хотя он не должен быть по моему, можете посмотреть?

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,10 @@
 import React from 'react'
-import Input from './components/Input'
 import Nav from './components/Nav'
 
 const App = () => {
   return (
-    <div className='fixed w-[100%] h-[100vh] bg-gradient-to-r from-sky-500 to-indigo-500'>
-      <div className='flex justify-center'>
-        <Nav/>
-        <Input />
-      </div>
-     
+    <div className=''>
+      <Nav/>
     </div>
   )
 }
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,20 +1,31 @@
 import React from 'react'
 
 const Nav = () => {
+
+    const [isOpen, setIsOpen] = React.useState(false)
+
+    const openMenu = () =>{
+        setIsOpen(!isOpen)
+    }
+
+
   return (
-   <div className='flex bg-white fixed items-center text-slate-900 text-[18px] font-medium tracking-wider mt-[50px] '>
-        <div className='flex items-center'>
-            <a href="#">
-              <img 
-                className='w-[160px]' 
-                src="https://img5.lalafo.com/i/posters/original/1e/98/755d0693f57026a127fcbf8c8a82.jpeg" alt="" />
-            </a>
-            <i class="fa-solid fa-bars hover:text-violet-700 cursor-pointer mr-[400px]"></i>
-        </div>
-        <div className='flex items-center'>
-            <i class="fa-solid fa-heart hover:text-violet-700 cursor-pointer	"></i>
-            <div className='mx-[20px] hover:text-violet-700 cursor-pointer	'>Войти / Регестрация</div>
-            <button className='bg-gradient-to-r from-purple-500 to-pink-500 text-white rounded-[10px] py-[3px] mr-[10px] px-[20px]'>Поддать обьявление</button>
+    <div className='flex justify-between px-[50px] py-[20px] items-center top-[0px] left-[0px] bg-indigo-800'>
+        <a href='#' className='text-[20px] font-serif'>Logotype</a>
+        <div className='relative w-[660px] text-end'>
+            <input 
+                type="text" 
+                placeholder='что вы ищете?...'
+                className={`rounded-l-lg bg-slate-200 px-[20px] py-[2px] outline-none absolute left-[710px] duration-150  ${isOpen ? 'left-[4px]' : ''}`}
+            />
+            <button
+                onClick={openMenu}
+                className={`bg-indigo-400 rounded-full py-[2px] px-[10px] outline-none duration-150 ${isOpen ? 'rounded-l-lg' : ''}`}
+            >
+                {isOpen ? <i class="fa-solid fa-x"></i> : <i class="fa-solid fa-magnifying-glass"></i>}
+            </button>
+            <a href="#" className='mx-[50px]'>Регистрация \ Вход</a>
+            <button className='py-[2px] bg-indigo-400 px-[10px] rounded-[10px]'>Подать обьявление</button>
         </div>
     </div>
   )
